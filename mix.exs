f388541760da2da64e50e58925df676e758a4b26
Marco Polita <marco.polita@kloeckner.com>
2022-08-12 15:15:08 +0200

    bumps dependencies

--- mix.exs
+++ mix.exs
@@ -32,10 +32,10 @@ defmodule HueSDK.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:finch, "~> 0.6.1"},
+      {:finch, "~> 0.13"},
       {:jason, "~> 1.2"},
       {:mdns, "~> 1.0"},
-      {:nimble_options, "~> 0.3.5"},
+      {:nimble_options, "~> 0.4"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
