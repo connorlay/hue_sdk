9da3675dc32e5952a6821ae5f5ef1501320b996f
Connor Lay (Clay) <connorlay@pm.me>
2022-08-13 11:30:15 -0700

    Release v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HueSDK.MixProject do
   def project do
     [
       app: :hue_sdk,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
